bff873a65cb5f98e7502ae0dbb7a41a43048a448
Md Sojib Ali <alisojib295@gmail.com>
2025-05-22 22:08:36 +0600

    env add

--- src/firebase/Firebase_config.js
+++ src/firebase/Firebase_config.js
@@ -1,19 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCN1OMZUTMy1hDuyBHRD0T6LRpwR7QzGfg",
-  authDomain: "gardening-community-client.firebaseapp.com",
-  projectId: "gardening-community-client",
-  storageBucket: "gardening-community-client.firebasestorage.app",
-  messagingSenderId: "177697919183",
-  appId: "1:177697919183:web:d5e5682cd960adba4527ba",
-  measurementId: "G-DNYTP05NPW",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
+  measurementId: import.meta.env.VITE_measurementId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
