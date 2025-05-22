0f689650538d7a15952a18840d01b87daeaeb7d6
Md Sojib Ali <alisojib295@gmail.com>
2025-05-23 01:15:13 +0600

    Gardentrslist grid add

--- src/Components/GardenersList/GardenersList.jsx
+++ src/Components/GardenersList/GardenersList.jsx
@@ -11,7 +11,7 @@ const GardenersList = () => {
         Explore Gardeners
       </h2>
 
-      <div className="container mx-auto grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      <div className="container mx-auto grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
         {gardeners.map((gardener) => (
           <GardenerCard key={gardener.id} gardener={gardener} />
         ))}
